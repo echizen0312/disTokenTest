83e23b232859512c870fee332ace5b5dbf0af8da
Izayoi Sakuya <sakuya0312@qq.com>
2018-12-27 11:28:50 +0800

    sakuya

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,18 @@
 import Router from 'vue-router' */
 const Home = () => import('@/components/Home').then(m => m.default)
 const AccountList = () => import('@/components/AccountList').then(m => m.default)
+const EOSTest = () => import('@/sakuya/EOSTest').then(m => m.default)
 
 const routes = [
-  { 
-    path: '/', 
-    name: 'home', 
+  {
+    path: '/',
+    name: 'home',
     component: Home,
     children: [
-      { 'path': '/AccountList', component: AccountList, 'name': 'AccountList' }
+      {'path': '/AccountList', component: AccountList, 'name': 'AccountList'},
+      {'path': '/EOSTest', component: EOSTest, 'name': 'EOSTest'}
     ]
   }
 ]
 
-export default routes;
+export default routes
---
+++ src/sakuya/EOSTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>EOSTestasd</div>
+</template>
+
+<script>
+  export default {
+    name: 'EOSTest',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+</style>
