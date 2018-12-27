ccc004364820e6a271f7c17a8980872a3f7a10dc
SilentKc <532511275@qq.com>
2018-12-27 11:34:49 +0800

    增加routeview

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="homeroute"><router-view ref="child"/></div>
     <mu-bottom-nav class="mu_bottom_nav" @change="Gotabbar" :value="nowIndex">
       <mu-bottom-nav-item title="钱包" icon="credit_card"></mu-bottom-nav-item>
       <mu-bottom-nav-item title="游戏" icon="videogame_asset"></mu-bottom-nav-item>
@@ -30,10 +31,9 @@
       Gotabbar (index) {
         const that = this
         that.nowIndex = index
-        console.log(index)
         switch (index) {
           case 0:
-            that.$router.push('/IndexHome')
+            that.$router.push('/AccountList')
             break
           case '1':
             console.log(123)
@@ -62,6 +62,14 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+.homeroute{
+  width: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 56px;
+  overflow: scroll;
+  background-color: #eeeeee;
+}
   .mu_bottom_nav {
     width: 100%;
     position: fixed;
